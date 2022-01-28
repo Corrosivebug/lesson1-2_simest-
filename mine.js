f15e60469ff23a20d54f74ab6756973188bcae8c
Corrosivebug <92105717+Corrosivebug@users.noreply.github.com>
2022-01-28 14:53:28 +0200

    lesson1
    2simestr

---
+++ mine.js
@@ -0,0 +1,32 @@
+
+// alert("Купить лецензию маинкрафт?") //функция выкидывает модальное окошко
+// let y = 25  //Создал переменную
+// let userName = "Вас зовут Макс?"
+// let admin_name = "Denis"
+// alert(userName)
+// alert("Адимин под имени: " + admin_name + " разре шил вам задонить?")
+// let a = 5; b = 6; c = "Den";
+// alert("Пройти контроль?")
+// alert("5 * 5 =" + y + "?")
+// alert("6 * 5 =" + y*a + "?")
+// alert("Задонатить:  " + (a + b)*y + "грн?")
+
+let userName = prompt("Как тебя зовут?")  //инпут
+// alet =prompt ("Привет " + userName + "!Чем займёмся?")
+// alert("Отличнаяя идея!")
+alert("Привет " + userName +"!")
+let qacshen = prompt(userName + " ты вериш в бога?")
+alert("Ну смотри мне...")
+console.log(userName) //в ф12
+const arr = [5, 6, "ParenMechti"] //список
+arr.forEach(i => console.log(i))  //масив илиже стречаная функиця для вывода в консоль
+
+
+
+
+
+
+
+
+
+
